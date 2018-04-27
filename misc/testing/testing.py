29730a8d4c09aae5ce1e1fa752f54752e596333a
Kevin Danikowski <kdanikowski@outlook.com>
2018-04-26 23:49:31 -0500

    thrid commit

--- misc/testing/testing.py
+++ misc/testing/testing.py
@@ -1,4 +1,7 @@
-import nltk
 
-nltk.download()
-print('Hello, world!')
+from nltk.tokenize import sent_tokenize, word_tokenize
+
+example_test = "hey, let us try to do something here today please"
+
+print(sent_tokenize(example_test))
+print(word_tokenize(example_test))
