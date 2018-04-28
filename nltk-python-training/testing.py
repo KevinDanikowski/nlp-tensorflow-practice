7c4d2568c9e860bdc15662961c45088a1a4c0250
Kevin Danikowski <kdanikowski@outlook.com>
2018-04-28 10:00:03 -0500

    finished 12

--- nltk-python-training/testing.py
+++ nltk-python-training/testing.py
@@ -8,15 +8,25 @@ documents = [(list(movie_reviews.words(fileid)), category)
 
 random.shuffle(documents)
 
-# print(documents[1]) # list of words
-
-all_words = [] #50k words or more
+all_words = [] 
 
 for w in movie_reviews.words():
     all_words.append(w.lower())
 
+all_words = nltk.FreqDist(all_words) #ordered from most to least common words
 
-all_words = nltk.FreqDist(all_words)
-# print(all_words.most_common(15)) #printing common words, most are punctuation though
+word_features = list(all_words.keys())[:3000]
 
-# print(all_words["stupid"]) # prints 253 because 253 times stupid is said
+def find_features(document):
+    words = set(document) #all words
+    features = {}
+    for w in word_features:
+        features[w] = (w in words) #boolean true / false, tells us if this review contains those top words
+        # true only if the review contains one of top 3000 words
+
+    return features
+
+print((find_features(movie_reviews.words('neg/cv000_29416.txt'))))
+
+featuresets = [(find_features(rev), category) for (rev, category) in documents]
+
